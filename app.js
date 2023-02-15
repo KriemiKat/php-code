5b78373cc0a74ed55cb36419a6e4569a294be37b
Katažina <katazina.fukalova@gmail.com>
2023-02-15 11:14:54 +0200

    Init commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  }
+// Math.floor(math.random() * 11 +1);
+  const arr=[];
+
+  for(let i=0; i <10; i++) {
+    arr.push(getRandomIntInclusive(1,11));
+  }
+  console.log(arr);
+
+  const arr2 = [...Array(10)].map(_=> getRandomIntInclusive(1,11));
+  console.log(arr2);
