90ef2e9ff7e0203ac017b5d4fe2610ae301848b9
Katažina <katazina.fukalova@gmail.com>
2023-02-16 14:17:37 +0200

    homerork

--- homework.js
+++ homework.js
@@ -1,34 +1,72 @@
 /*Programiškai ridenkite du žaidimo kauliukus- sugeneruokite du atsitiktinius skaičius nuo 1 iki 6 Jeigu kauliukų suma didesnė nei 8 jūs laimėjote, priešingu atveju pralošėte. Išveskite atsakymą, kuriame būtų abiejų kauliukų reikšmės ir išvada laimėjote ar pralošėte. */
 
-    let x = Math.floor(Math.random() * 6) + 1;
+  /*  let x = Math.floor(Math.random() * 6) + 1;
     console.log(x);
     let y = Math.floor(Math.random() * 6) + 1;
     console.log(y);
 
     if(x + y >= 8) {
-        console.log('Sveikiname, Jus laimejote!')    }
+        console.log((x), (y), 'Sveikiname, Jus laimejote!')    }
     else {
-        console.log('Apgailestaujame, Jus pralosete')    }
+        console.log((x), (y), 'Apgailestaujame, Jus pralosete')    }
+
+        console.log('-----------------------'); */
 
 /*Gyveno du katinukai, Pilkis ir Murklys. Jų svoriai kilogramais buvo atsitiktiniai dydžiai nuo 3 iki 6. Parašyti programą, kuri išvestų katinukų svorius ir apskaičiuotų, kuris katinukas yra lengvesnis. Atsakymas turi būti katinukų vardai su jų svoriais ir lengvesnio katinuko vardas. Jeigu katinukai sveria vienodai, vietoj katinuko vardo parašykite, kad “katinukų svoriai vienodi”. */
 
-let  pilkioSvoris= Math.floor(Math.random() * 4) + 3;
-console.log(pilkioSvoris);
-let  murklioSvoris= Math.floor(Math.random() * 4) + 3;
-console.log(murklioSvoris);
+function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-//function kurisLengvsenis (pilkioSvoris, murklioSvoris) {
- if (pilkioSvoris > murklioSvoris)
-    {console.log('Murklys(murklioSvoris), Pilkis(pilkioSvoris), Murklys')}
-else if (pilkioSvoris < murklioSvoris)
-    {console.log('Pilkis(pilkioSvoris), Murklys(murklioSvoris), Pilkis')}
+let Pilkis = (getRandom(3, 6));
+let Murklys = (getRandom(3, 6));
+console.log(Pilkis, Murklys);
+
+  if (Pilkis>Murklys)
+    {console.log ('Murklys' + '=' + Murklys, 'Pilkis' + '=' + Pilkis + ' ' + 'Murklys lengvesnis')}
+else if (Pilkis<Murklys)
+    {console.log('Murklys' + '=' + Murklys, 'Pilkis' + '=' + Pilkis + ' ' + 'Pilkis lengvesnis')}
 else {console.log('katinukų svoriai vienodi')}
-//}
 
-//kurisLengvsenis ()
 
 /*Nojus pasiruošė potvyniui ir nusipirko dvi valtis. Vienoje telpa 15 katinukų, kitoje 15 karvių (katinukus galima sodinti tik į katinukų valtis, o karves tik į karvių, maišyti negalima) Prasidėjus liūčiai pas Nojų atėjo atsitiktinis skaičius nuo 0 iki 30 katinukų ir toks pats atsitiktinis skaičius karvių. Išveskite atėjusių katinukų ir karvių skaičių ir ar Nojus galės juos sutalpinti į valtis. Jeigu netelpa tik katinukai arba tik karvės vistiek išveskite “Netelpa”. Kas konkrečiai netelpa išvedinėti nereikia. “Telpa” išveskite tik tuo atveju jeigu ir katinukai ir karvės telpa. */
 
+function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+let katinai = (getRandom(0, 30));
+let karves = (getRandom(0, 30));
+console.log(katinai, karves);
+
+if (katinai<=15 && karves<=15) {
+    console.log('telpa')
+}
+else {
+    console.log('netelpa')
+}
+
+
 /*Antanas nusipirko naują butą ir pinigų jam liko nedaug. Dabar jis niekaip negali apsispręsti ką pirmiausiai pirkti: televizorių, skalbimo mašiną ar šaldytuvą. Todėl nusprendžia ridenti kauliuką (atsitiktinis skaičius nuo 1 iki 6) ir jeigu iškris 1 arba 5 pirkti televizorių, jeigu 3 arba 4 pirkti skalbimo mašiną ir jeigu 2 arba 6 - šaldytuvą. Padėkite Antanui apsispręsti. Ridenkite kauliuką ir parašykite atsakymą kokį daiktą jam pirkti. */
+function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+let x = (getRandom(1, 6));
+console.log(x)
+
+if (x === 1 || x === 5) {
+    console.log('pirkti televizoriu')
+} else if (x === 3 || x === 4) {
+    console.log('pirkti skalbimo masina')
+} else {
+    console.log('pirkti saltytuvas')
+}
+
 
 /*(BOSO lygis) Sugeneruokite tris atsitiktinius skaičius nuo 1 iki 7. Skaičius atspausdinkite nuo mažiausio iki didžiausio. Pavyzdžiui: sugeneravus 4, 2, 4 juos reikia atspausdinti tokia tvarka: 2 4 4; */
